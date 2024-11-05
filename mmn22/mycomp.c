db2c0b1a56817731457914ca030370b6ff220a1a
Afek Banyas <afek10@gmail.com>
2024-11-05 21:14:09 +0200

    start errors

--- mmn22/mycomp.c
+++ mmn22/mycomp.c
@@ -4,6 +4,7 @@
 void execute_command(char* command);
 complex* find_comp (char comp);
 void clean_input(char* input);
+int checkCommand(char ch, double real, double img, int numOfVar, char* cmd);
 
 complex A = {0, 0}, B = {0, 0}, C = {0, 0}, D = {0, 0}, E = {0, 0}, F = {0, 0};
 
@@ -33,55 +34,139 @@ int main() {
 void execute_command(char* command) 
 {
     char comp1, comp2;
-    double real, imag, test;
+    double real = -1, imag = -1;
+    int numOfVar = 0;
 
-    if (sscanf(command, "read_comp %c, %lf, %lf", &comp1, &real, &imag) == 3) 
+    if (numOfVar = sscanf(command, "read_comp %c, %lf, %lf", &comp1, &real, &imag)) 
     {
+        if (find_comp(comp1) == NULL) 
+        {
+            printf("Undefined complex variable\n");
+            return;
+        }
+        if (numOfVar < 3) 
+        {
+            printf("Missing parameter\n");
+            return;
+        }
         printf("Setting complex %c to %.2f + (%.2f)i\n", comp1, real, imag);
         read_comp(find_comp(comp1), real, imag);
     } 
-    else if (sscanf(command, "print_comp %c", &comp1) == 1) 
+    else if (numOfVar = sscanf(command, "print_comp %c", &comp1)) 
     {
+        if (find_comp(comp1) == NULL) 
+        {
+            printf("Undefined complex variable\n");
+            return;
+        }
+        if (numOfVar < 1) 
+        {
+            printf("Missing parameter\n");
+            return;
+        }
         printf("Printing complex %c\n", comp1);
         print_comp(*find_comp(comp1)); 
     } 
-    else if (sscanf(command, "add_comp %c, %c", &comp1, &comp2) == 2) 
+    else if (numOfVar = sscanf(command, "add_comp %c, %c", &comp1, &comp2)) 
     {
-        printf("Add complex %c to complex %c\n", comp1, &comp2);
+        if (find_comp(comp1) == NULL || find_comp(comp2) == NULL) 
+        {
+            printf("Undefined complex variable\n");
+            return;
+        }
+        if (numOfVar < 2) 
+        {
+            printf("Missing parameter\n");
+            return;
+        }
+        printf("Add complex %c to complex %c\n", comp1, comp2);
         add_comp(*find_comp(comp1), *find_comp(comp2)); 
     }
-    else if (sscanf(command, "sub_comp %c, %c", &comp1, &comp2) == 2) 
+    else if (numOfVar = sscanf(command, "sub_comp %c, %c", &comp1, &comp2)) 
     {
-        printf("Sub complex %c from complex %c\n", comp1, &comp2);
+        if (find_comp(comp1) == NULL || find_comp(comp2) == NULL) 
+        {
+            printf("Undefined complex variable\n");
+            return;
+        }
+        if (numOfVar < 2) 
+        {
+            printf("Missing parameter\n");
+            return;
+        }
+        printf("Sub complex %c from complex %c\n", comp2, comp1);
         sub_comp(*find_comp(comp1), *find_comp(comp2)); 
     }
-    else if (sscanf(command, "mult_comp_real %c, %lf", &comp1, &real) == 2) 
+    else if (numOfVar = sscanf(command, "mult_comp_real %c, %lf", &comp1, &real)) 
     {
-        printf("Multiply complex %c by real number %.2f\n", &comp1, real);
+        if (find_comp(comp1) == NULL) 
+        {
+            printf("Undefined complex variable\n");
+            return;
+        }
+        if (numOfVar < 2) 
+        {
+            printf("Missing parameter\n");
+            return;
+        }
+        printf("Multiply complex %c by real number %.2f\n", comp1, real);
         mult_comp_real(*find_comp(comp1), real); 
     }
-    else if (sscanf(command, "mult_comp_img %c, %lf", &comp1, &imag) == 2) 
+    else if (numOfVar = sscanf(command, "mult_comp_img %c, %lf", &comp1, &imag)) 
     {
-        printf("Multiply complex %c by imaginary number %.2f\n", comp1, imag);
+        if (find_comp(comp1) == NULL) 
+        {
+            printf("Undefined complex variable\n");
+            return;
+        }
+        if (numOfVar < 2) 
+        {
+            printf("Missing parameter\n");
+            return;
+        }
+        printf("Multiply complex %c by imaginary number %.2fi\n", comp1, imag);
         mult_comp_img(*find_comp(comp1), imag); 
     }
-    else if (sscanf(command, "mult_comp_comp %c, %c", &comp1, &comp2) == 2) 
+    else if (numOfVar = sscanf(command, "mult_comp_comp %c, %c", &comp1, &comp2)) 
     {
-        printf("Multiply complex %c by imaginary number %c\n", &comp1, &comp2);
+        if (find_comp(comp1) == NULL || find_comp(comp2) == NULL) 
+        {
+            printf("Undefined complex variable\n");
+            return;
+        }
+        if (numOfVar < 2) 
+        {
+            printf("Missing parameter\n");
+            return;
+        }
+        printf("Multiply complex %c by complex %c\n", comp1, comp2);
         mult_comp_comp(*find_comp(comp1), *find_comp(comp2)); 
     }
-    else if (sscanf(command, "abs_comp %c", &comp1) == 1)
+    else if (numOfVar = sscanf(command, "abs_comp %c", &comp1))
     {
-        printf("Calculate the absolute value of complex %c\n", &comp1);
+        if (find_comp(comp1) == NULL) 
+        {
+            printf("Undefined complex variable\n");
+            return;
+        }
+        if (numOfVar < 1) 
+        {
+            printf("Missing parameter\n");
+            return;
+        }
+        printf("Calculate the absolute value of complex %c\n", comp1);
         abs_comp(*find_comp(comp1)); 
     }  
     else 
     {
-        printf("Error: Unknown command or invalid format.\n");
+        printf("Undefined command name\n");
     }
 }
 
+int checkCommand(char ch, double real, double img, int numOfVar, char* cmd)
+{
 
+}
 complex* find_comp (char comp)
 {
     switch (comp)
